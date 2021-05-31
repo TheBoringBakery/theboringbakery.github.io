32d3d83a3a2d3ef35c73a4b1ccc25f0a37594212
Feka7 <feka7@hotmail.it>
2021-05-31 19:47:13 +0200

    change public folder

--- src/components/parallax/Home2.js
+++ src/components/parallax/Home2.js
@@ -24,7 +24,7 @@ export default function Home2() {
       <div className="parent1">
       <Image
         alt="test"
-        src="/moon/MoonCaker_3d_try2/mooncaker_bg_3d.png"
+        src="/moon/mooncaker_bg_3d2.png"
         className="parent2"
         style={{
           transform: `translateY(${offset * (0.3)}px)
@@ -34,7 +34,7 @@ export default function Home2() {
        />
        <Image
          alt="test"
-         src="/moon/MoonCaker_3d_try2/mooncaker_mid_3d.png"
+         src="/moon/mooncaker_mid_3d2.png"
          className="parent2"
          style={{
            transform: `translateY(${(offset - 1060) * (-0.3)}px)
@@ -43,7 +43,7 @@ export default function Home2() {
        />
       <Image
         alt="test"
-        src="/moon/MoonCaker_3d_try2/mooncaker_fg_3d.png"
+        src="/moon/mooncaker_fg_3d2.png"
         className="parent2"
         style={{
           transform: `translateY(${(offset - 200) * (0.6)}px)
