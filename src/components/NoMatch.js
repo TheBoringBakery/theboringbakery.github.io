d61faeeca8f411b8a68d951795521711bd8c078a
Feka7 <feka7@hotmail.it>
2021-06-16 00:22:40 +0200

    page 404

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,9 +1,27 @@
 import React from 'react'
-//classe utilizzata quando la ricerca non da nessun risultato
+import {
+    Container,
+    Row,
+    Col,
+    Image
+ } from 'react-bootstrap';
+ import './Clash.css';
+
 
 function NoMatch() {
   return(
-    <p>Sembra non esserci nulla qui...</p>
+      <div className="main" style={{minHeight: '920px'}}>
+        <Container>
+            <Row className="justify-content-center spacer-simple-page">
+                <h4 className="text-white text-center">Oops, there doesn't seem to be anything here...
+                    <Image
+                    src="/zoe_sad.png"
+                    width="370"
+                    fluid />
+                </h4>
+            </Row>
+        </Container>
+      </div>
   );
 }
 
